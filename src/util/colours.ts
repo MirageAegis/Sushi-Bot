2985e0e9f0dc142debc3f64f5ae4b04fa3eda4ce
Mira <miraegisswe@gmail.com>
2023-07-08 14:48:57 +0200

    #18 Add colour hexes

--- src/util/colours.ts
+++ src/util/colours.ts
@@ -4,3 +4,6 @@
 
 export const BLUE: number = 0x3498DB;
 export const MAGENTA: number = 0xE91E63;
+export const RED: number = 0xE74C3C;
+export const YELLOW: number = 0xFEE75C;
+export const GREEN: number = 0x2ECC71;
