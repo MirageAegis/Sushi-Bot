1bedfc66df8d8ff0a8e385dbca6dd390c3740282
Mira <miraegisswe@gmail.com>
2023-08-01 01:17:23 +0200

    chore: Correct copyright notice again

--- src/util/channels.ts
+++ src/util/channels.ts
@@ -2,7 +2,6 @@
  * MIT License
  *
  * Copyright (c) 2022-present Mirage Aegis
- * Copyright (c) 2023-present Zahatikoff
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
