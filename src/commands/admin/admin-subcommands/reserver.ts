b072c71589e345a7de0240df4b7cad9bcff72ad7
Mira <miraegisswe@gmail.com>
2023-08-09 23:40:18 +0200

    #24 Implement admin reserver command

---
+++ src/commands/admin/admin-subcommands/reserver.ts
@@ -0,0 +1,117 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023-present Mirage Aegis
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { 
+    SlashCommandSubcommandBuilder, EmbedBuilder, ChatInputCommandInteraction,
+    ButtonBuilder, ButtonStyle, ActionRowBuilder, Message, CollectorFilter,
+    ButtonInteraction, MessageComponentInteraction, InteractionResponse
+} from "discord.js";
+import { Subcommand } from "../../../util/command-template.js";
+import { defaultErrorHandler } from "../../../util/error-handler.js";
+import { refreshServers } from "../../../util/refresh.js";
+
+/*
+ * A command for the administrators of Sushi Bot to use for
+ * reloading the bot's joined servers
+ */
+const name: string = "reserver";
+
+export const command: Subcommand = {
+    // Command headers
+    data: new SlashCommandSubcommandBuilder()
+        .setName(name)
+        .setDescription("Reloads the bot's servers on demand"),
+
+    // Command execution
+    async execute(ctx: ChatInputCommandInteraction): Promise<void> {
+        const yes: ButtonBuilder = new ButtonBuilder()
+            .setCustomId("yes")
+            .setLabel("Yes")
+            .setStyle(ButtonStyle.Success);
+        
+        const no: ButtonBuilder = new ButtonBuilder()
+            .setCustomId("no")
+            .setLabel("No")
+            .setStyle(ButtonStyle.Danger);
+
+        const row: ActionRowBuilder<ButtonBuilder> = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(yes, no);
+
+        const prompt: InteractionResponse = await ctx.reply({
+            content: "Reload servers? This may take a while",
+            components: [row]
+        });
+
+        // Check if the user pressing the button is the same user
+        const check: CollectorFilter<[ButtonInteraction]> = (m: ButtonInteraction): boolean => m.user.id === ctx.user.id;
+
+        // Disable the buttons to signify that the interaction has ended
+        row.components.forEach(c => c.setDisabled(true));
+
+        try {
+            const confirmation: MessageComponentInteraction = await prompt.awaitMessageComponent({ filter: check, time: 10_000 });
+
+            // End the interaction as soon as a button is pressed
+            await confirmation.update({
+                components: [row]
+            });
+
+            // If the user selected no, abort blacklisting
+            if (confirmation.customId === "no") {
+                await ctx.followUp("Blacklisting denied, aborting opertation");
+                return;
+            }
+        } catch (e) {
+            // End the interaction upon timeout
+            await prompt.edit({
+                components: [row]
+            });
+            // If the interaction timed out, abort as well
+            await ctx.followUp("Interaction timed out, aborting operation");
+            return;
+        }
+
+        // Proceed if the user pressed yes
+        const report: Message = await ctx.followUp(
+            `Commencing server reload...\n` +
+            `Actions will be logged in <#${process.env.LOGS_CHANNEL_ID}>`
+        );
+
+        await refreshServers(ctx.client);
+
+        await report.edit("Reload complete");
+    },
+
+    // Error handler
+    error: defaultErrorHandler,
+
+    // Help command embed
+    help: new EmbedBuilder()
+        .setTitle("Reserver")
+        .setDescription(
+            "An administrative command that reloads the servers Sushi Bot is in, leaving every server that isn't eligible")
+        .addFields(
+            { name: "Format", value: `\`/admin ${name}\`` }
+        )
+};
