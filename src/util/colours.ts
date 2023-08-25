9bf486e61e2590eddc59bb40833af900d807df41
Mira <miraegisswe@gmail.com>
2023-08-25 22:44:38 +0200

    enhance: Make colours pop more

--- src/util/colours.ts
+++ src/util/colours.ts
@@ -2,11 +2,11 @@
  * Utility module containing colour hexes
  */
 
-export const BLUE: number = 0x3498DB;
-export const MAGENTA: number = 0xE91E63;
-export const RED: number = 0xE74C3C;
-export const YELLOW: number = 0xFEE75C;
-export const GREEN: number = 0x2ECC71;
-export const PURPLE: number = 0x9B59B6;
-export const ORANGE: number = 0xE67E22;
-export const TEAL: number = 0x1ABC9C;
+export const BLUE: number = 0x2C2CFF;
+export const MAGENTA: number = 0xFF00FF;
+export const RED: number = 0xEE0000;
+export const YELLOW: number = 0xEED200;
+export const GREEN: number = 0x00AA00;
+export const PURPLE: number = 0x9000FF;
+export const ORANGE: number = 0xEE6700;
+export const TEAL: number = 0x008080;
