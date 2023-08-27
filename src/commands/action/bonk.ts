ac4e7eb4796bcd38d149ff75666a99fa9fe7cfae
Zaha <zahatikoff@gmail.com>
2023-08-27 23:25:14 +0300

    fix: Update year in license

--- src/commands/action/bonk.ts
+++ src/commands/action/bonk.ts
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022-present Zahatikoff
+ * Copyright (c) 2023-present Zahatikoff
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -35,7 +35,7 @@ import { TenorSingleton } from "../../util/tenor-utils.js";
 const name: string = "bonk";
 
 export const command: Command = {
-    data: <SlashCommandBuilder>new SlashCommandBuilder()
+    data: <SlashCommandBuilder> new SlashCommandBuilder()
         .setName(name)
         .setDescription("Bonk your friends ... or your enemies.\n ***the bonk*** *never* misses")
         .setDMPermission(false)
