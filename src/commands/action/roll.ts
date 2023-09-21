b8fe2a66c001aacd0d34e81dc6dd30cf5d00edf0
Mira <miraegisswe@gmail.com>
2023-09-21 02:03:54 +0200

    enhance: Enable muliple dice per roll

--- src/commands/action/roll.ts
+++ src/commands/action/roll.ts
@@ -37,6 +37,10 @@ const MIN_SIDES: number = 2;
 const MAX_SIDES: number = 1000;
 // The default dice has 6 sides
 const DEFAULT_SIDES: number = 6;
+// The minimum amount of dice to roll
+const MIN_AMOUNT: number = 1;
+// The maximum amount of dice to roll
+const MAX_AMOUNT: number = 10;
 // 1.5 seconds delay per roll
 const ROLL_DELAY: number = 1500;
 
@@ -46,13 +50,19 @@ export const command: Command = {
     // Command headers
     data: <SlashCommandBuilder> new SlashCommandBuilder()
         .setName(name)
-        .setDescription("Roll a dice")
+        .setDescription("Roll a die")
         .setDMPermission(false)
         .addIntegerOption(o =>
             o.setName("sides")
-                .setDescription("The amount of sides the dice should have, defaults to 6")
+                .setDescription("The amount of sides the die should have, defaults to 6")
                 .setMinValue(MIN_SIDES)
                 .setMaxValue(MAX_SIDES)
+        )
+        .addIntegerOption(o =>
+            o.setName("amount")
+                .setDescription("The amount of dice to throw, defaults to 1")
+                .setMinValue(MIN_AMOUNT)
+                .setMaxValue(MAX_AMOUNT)
         ),
     
     // Command exacution
@@ -60,15 +70,29 @@ export const command: Command = {
         // Default values for parameters
 
         const sides: number = ctx.options.getInteger("sides") ?? DEFAULT_SIDES;
+        const amount: number = ctx.options.getInteger("amount") ?? MIN_AMOUNT;
+        const multi: boolean = amount > MIN_AMOUNT;
         const user: User = ctx.user;
 
-        // Roll a number between 1~sides
-        const roll: number = Math.ceil(Math.random() * sides);
+        // Roll a numbers between 1~sides
+        let roll: number;
+        let multiResponse: string = `${user} rolled ${amount} D${sides}s, which resulted in the following:\n`;
+        for (let i = 0; i < amount; i++) {
+            roll = Math.ceil(Math.random() * sides);
+            multiResponse += `- ${Math.ceil(Math.random() * sides)}\n`;
+        }
 
-        // The
-        const response = await ctx.reply(`${user} rolls a D${sides}...\n`);
+        // The response saying that the dice is being rolled
+        const response = await ctx.reply(
+            `${user} rolls ${multi ? amount : "a"} D${sides}${multi ? "s" : ""}...\n`
+        );
 
         setTimeout(async () => {
+            if (multi) {
+                await response.edit(multiResponse);
+                return;
+            }
+
             await response.edit(
                 `${user} rolled ${roll} on a D${sides}!`
             );
@@ -85,11 +109,16 @@ export const command: Command = {
             "A command for rolling a dice. You can specify the number of sides that the dice should have"
         )
         .addFields(
-            { name: "Format", value: `\`/${name} [sides]\`` },
+            { name: "Format", value: `\`/${name} [sides] [amount]\`` },
             {
                 name: "[sides]",
-                value: "Optional parameter. The amount of sides the dice should have, defaults to 6. " +
+                value: "Optional parameter. The amount of sides the die should have, defaults to 6. " +
                        "Accepts values between 2 and 1000"
+            },
+            {
+                name: "[amount]",
+                value: "Optional parameter. The amount of dice to roll, defaults to 1. " +
+                       "Accepts values between 1 and 10"
             }
         )
 };
