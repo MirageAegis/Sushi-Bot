e09bdb3108320f8c868663f4d16ce0065a056383
Mira <miraegisswe@gmail.com>
2024-03-26 01:09:43 +0100

    #11 Define base weapons

---
+++ src/rpg/weapons/daggers/iron-dagger.ts
@@ -0,0 +1,66 @@
+/* 
+ * MIT License
+ * 
+ * Copyright (c) 2023-present Mirage Aegis
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { Player } from "../../../schemas/player";
+import { AttackForecast, AttackTypes, DamageTypes } from "../../types/attack";
+import { Unit } from "../../types/unit";
+import { Daggers, Weapon, WeaponClasses } from "../../types/weapon";
+import {
+    calculateAttackSpeed, calculateAvoidance, calculateCritAvoidance, calculateCritStat,
+    calculateHitStat
+} from "../../util/calculations";
+
+export const weapon: Weapon<WeaponClasses.Dagger> = {
+    name: Daggers.Iron,
+    description: "An iron dagger. This dagger is a standard for every new assassin",
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    unlock(player: Player): void {
+        // Nothing further to unlock
+        return;
+    },
+    class: WeaponClasses.Dagger,
+    type: AttackTypes.Normal,
+    count: 1,
+    damage: DamageTypes.Physical,
+    might: 2,
+    weight: 1,
+    hit: 95,
+    crit: 5,
+    effects: [],
+    use(attacker: Unit): AttackForecast {
+        const attackSpeed: number = calculateAttackSpeed(this.damage, this.weight, attacker);
+        return {
+            attacker: attacker,
+            might: attacker.stats.strength + this.might,
+            count: this.count,
+            type: this.type,
+            attackSpeed: attackSpeed,
+            hit: calculateHitStat(this.hit, this.damage, attacker),
+            crit: calculateCritStat(this.crit, attacker),
+            avo: calculateAvoidance(attackSpeed, attacker),
+            critAvo: calculateCritAvoidance(attacker),
+            effects: this.effects
+        };
+    }
+};
